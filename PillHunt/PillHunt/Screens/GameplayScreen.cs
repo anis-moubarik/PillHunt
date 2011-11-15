3bcdbbe139b42f0e17d8c1aa7d7a28e62f9f40e3
SugarPwr <simo.sahlstedt@helsinki.fi>
2011-11-15 20:50:20 +0200

    cleaning up the gameplay, Pills-class created

--- PillHunt/PillHunt/Screens/GameplayScreen.cs
+++ PillHunt/PillHunt/Screens/GameplayScreen.cs
@@ -18,22 +18,23 @@ namespace PillHunt
         SpriteFont gameFont;
         SpriteFont font;
 
-        Texture2D awesomeFace;
-        Texture2D pill;
-        Texture2D dimmer;
-        Texture2D gameOver;
-        Texture2D bg;
+        Texture2D awesomeTexture;
+        Texture2D pillTexture;
+        Texture2D dimmerTexture;
+        Texture2D goTexture;
+        Texture2D bgTexture;
 
         Player player1;
         Player player2;
+        Pills pills;
         Timer clock;
         FPS fps;
 
-        Dictionary<Pill, Rectangle> pills;
-        List<Pill> toBeRemoved;
-
-        bool endGame;
+        bool gameEnds;
         float pauseAlpha;
+
+        int screenWidth;
+        int screenHeight;
 
 
         public GameplayScreen()
@@ -46,28 +47,17 @@ namespace PillHunt
             //sets the escape-key as the pause-button
             pauseAction = new InputAction(new Keys[] { Keys.Escape }, true);
 
+            screenWidth = 800;
+            screenHeight = 600;
+
             player1 = new Player(0, 0);
-            player2 = new Player(300, 300);
+            player2 = new Player(screenWidth - 32, screenHeight - 32);
             clock = new Timer();
-            fps = new FPS(800);
+            fps = new FPS(screenWidth);
+            pills = new Pills(100, screenWidth, screenHeight, 32);
 
-            endGame = false;
+            gameEnds = false;
 
-        }
-
-        //creates all the pills to the game and adds them and their (random) locations to a dictionary
-        public void createPills()
-        {
-
-            Random random = new Random();
-            int maxX = ScreenManager.GraphicsDevice.Viewport.Width - pill.Width;
-            int maxY = ScreenManager.GraphicsDevice.Viewport.Height - pill.Height;
-            pills = new Dictionary<Pill, Rectangle>();
-
-            for (int i = 0; i < 100; i++)
-            {
-                pills.Add(new Pill(), new Rectangle(random.Next(maxX), random.Next(maxY), 32, 32));
-            }
         }
 
         //activates the game by loading all the needed images etc.
@@ -75,6 +65,7 @@ namespace PillHunt
             {
 
             if (!instancePreserved)
+
                 {
 
                 if (content == null)
@@ -83,16 +74,14 @@ namespace PillHunt
                     }
                 
                 gameFont = content.Load<SpriteFont>("gamefont");
-                awesomeFace = content.Load<Texture2D>("awesome");
-                pill = content.Load<Texture2D>("pill");
+                awesomeTexture = content.Load<Texture2D>("awesome");
+                pillTexture = content.Load<Texture2D>("pill");
                 font = content.Load<SpriteFont>("FPS");
-                dimmer = content.Load<Texture2D>("dimmer");
-                gameOver = content.Load<Texture2D>("gameover");
-                bg = content.Load<Texture2D>("bg");
-                createPills();
+                dimmerTexture = content.Load<Texture2D>("dimmer");
+                goTexture = content.Load<Texture2D>("gameover");
+                bgTexture = content.Load<Texture2D>("bg");
 
                 Thread.Sleep(1000);
-
                 ScreenManager.Game.ResetElapsedTime();
 
                 }
@@ -114,6 +103,7 @@ namespace PillHunt
 
         public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
         {
+
             base.Update(gameTime, otherScreenHasFocus, false);
 
             // Gradually fade in or out depending on whether we are covered by the pause screen.
@@ -123,54 +113,41 @@ namespace PillHunt
                 pauseAlpha = Math.Max(pauseAlpha - 1f / 32, 0);
 
             if (IsActive)
+
             {
-                if (clock.getTime() > 0)
+
+                //game ends if the time runs out or all the pills are eaten
+
+                if (clock.getTime() > 0 && !pills.isEmpty())
                 {
                     clock.decreaseTime(gameTime.ElapsedGameTime.TotalSeconds);
                 }
-
+                
                 else
                 {
-                    clock.zero();
-                    endGame = true;
+                    gameEnds = true;
                 }
 
-                if (!endGame)
+
+                if (!gameEnds)
+
                 {
 
-                    toBeRemoved = new List<Pill>();
                     KeyboardState keyState = Keyboard.GetState();
 
                     movement(keyState);
                     fps.calculateFPS(gameTime);
 
-                    //checks if either of the players intersects with a piller
-                    //and if they do then the player gets a score and the piller is removed
-                    foreach (KeyValuePair<Pill, Rectangle> pair in pills)
-                    {
-                        if (player1.getPosition().Intersects(pair.Value))
-                        {
-                            player1.increaseScore();
-                            toBeRemoved.Add(pair.Key);
-                        }
-
-                        if (player2.getPosition().Intersects(pair.Value))
-                        {
-                            player2.increaseScore();
-                            toBeRemoved.Add(pair.Key);
-                        }
-                    }
-
-                    //removes the eaten pillers
-                    foreach (Pill pill in toBeRemoved)
-                    {
-                        pills.Remove(pill);
-                    }
+                    //counts new scores for both players and removes all the eaten pills
+                    player1.increaseScore(pills.countIntersections(player1.getPosition()));
+                    player2.increaseScore(pills.countIntersections(player2.getPosition()));
 
                 }
+
             }
         }
 
+        // --- movementille oma luokka ---
         private void movement(KeyboardState keyState)
         {
 
@@ -233,8 +210,8 @@ namespace PillHunt
             player2.slowDown();
             }
 
-            int maxX = ScreenManager.GraphicsDevice.Viewport.Width - awesomeFace.Width;
-            int maxY = ScreenManager.GraphicsDevice.Viewport.Height - awesomeFace.Height;
+            int maxX = ScreenManager.GraphicsDevice.Viewport.Width - awesomeTexture.Width;
+            int maxY = ScreenManager.GraphicsDevice.Viewport.Height - awesomeTexture.Height;
             int minY = 0;
             int minX = 0;
 
@@ -279,45 +256,32 @@ namespace PillHunt
             SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
             ScreenManager.GraphicsDevice.Clear(Color.White);
 
-            Rectangle dim = new Rectangle(0, 0, 800, 600);
-            int maxWidth = ScreenManager.GraphicsDevice.Viewport.Width;
-            int maxHeight = ScreenManager.GraphicsDevice.Viewport.Height;
-
             spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
 
-            spriteBatch.Draw(bg, new Vector2(0, 0), Color.White);
-
-            if (!endGame)
-            {
-                foreach (KeyValuePair<Pill, Rectangle> pair in pills)
-                {
-                    pair.Key.draw(spriteBatch, pill, pair.Value);
-                }
-            }
-
-            
+            spriteBatch.Draw(bgTexture, new Vector2(0, 0), Color.White);
+                
+            pills.draw(spriteBatch, pillTexture);        
             clock.draw(spriteBatch, font);
-            player1.draw(spriteBatch, awesomeFace, Color.Yellow);
-            player2.draw(spriteBatch, awesomeFace, Color.Red);
+            player1.draw(spriteBatch, awesomeTexture, Color.Yellow);
+            player2.draw(spriteBatch, awesomeTexture, Color.Red);
             fps.draw(spriteBatch, font);
 
+            spriteBatch.DrawString(font, "Player 1: " + player1.getScore(), new Vector2(screenWidth - 100, 20), Color.White);
+            spriteBatch.DrawString(font, "Player 2: " + player2.getScore(), new Vector2(screenWidth - 100, 40), Color.White);
 
-            if (endGame)
+
+            if (gameEnds)
             {
+                Rectangle dim = new Rectangle(0, 0, screenWidth, screenHeight);
                 Vector2 mid = new Vector2((float)ScreenManager.GraphicsDevice.Viewport.Width / 2, (float)ScreenManager.GraphicsDevice.Viewport.Height / 2);
-                Vector2 goPos = new Vector2((float)gameOver.Width / 2, (float)gameOver.Height / 2);
-                spriteBatch.Draw(dimmer, dim, new Color(new Vector4(1f, 1f, 1f, 0.5f)));
-                spriteBatch.Draw(gameOver, mid, null, Color.White, 0f, goPos, 1f, SpriteEffects.None, 1f);
+                Vector2 goPos = new Vector2((float)goTexture.Width / 2, (float)goTexture.Height / 2);
+                spriteBatch.Draw(dimmerTexture, dim, new Color(new Vector4(1f, 1f, 1f, 0.5f)));
+                spriteBatch.Draw(goTexture, mid, null, Color.White, 0f, goPos, 1f, SpriteEffects.None, 1f);
                 spriteBatch.DrawString(font, "Game Over", new Vector2(mid.X - 50, mid.Y - 50), Color.Black);
                 spriteBatch.DrawString(font, "Player 1 score: " + player1.getScore(), new Vector2(mid.X - 50, mid.Y - 20), Color.Black);
                 spriteBatch.DrawString(font, "Player 2 score: " + player2.getScore(), new Vector2(mid.X - 50, mid.Y - 0), Color.Black);
 
             }
-
-
-            spriteBatch.DrawString(font, "Player 1: " + player1.getScore(), new Vector2(maxWidth - 100, 20), Color.White);
-            spriteBatch.DrawString(font, "Player 2: " + player2.getScore(), new Vector2(maxWidth - 100, 40), Color.White);
-
 
             spriteBatch.End();
 
@@ -325,12 +289,9 @@ namespace PillHunt
             if (TransitionPosition > 0 || pauseAlpha > 0)
             {
                 float alpha = MathHelper.Lerp(1f - TransitionAlpha, 1f, pauseAlpha / 2);
-
                 ScreenManager.FadeBackBufferToBlack(alpha);
             }
         }
-
-        
 
     }
 }
