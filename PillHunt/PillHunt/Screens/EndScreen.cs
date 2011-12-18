cf6e6a4342758732bf0f6927c3dfefd567707b62
SugarPwr <simo.sahlstedt@helsinki.fi>
2011-12-18 02:35:33 +0200

    map2 edit

--- PillHunt/PillHunt/Screens/EndScreen.cs
+++ PillHunt/PillHunt/Screens/EndScreen.cs
@@ -62,16 +62,16 @@ namespace PillHunt
             if (tie)
                 {
                 spriteBatch.DrawString(font, "The game ended in a tie!", new Vector2(mid.X - 110, mid.Y - 20), Color.Black);
-                spriteBatch.DrawString(font, "Both players ate " + winner.getScore() + " pills.", new Vector2(mid.X - 110, mid.Y), Color.Black);
+                spriteBatch.DrawString(font, "Both players ate " + winner.getScore() + " pills.", new Vector2(mid.X - 105, mid.Y), Color.Black);
                 }
 
             else
                 {
                 spriteBatch.DrawString(font, winner.getName() + " ate " + winner.getScore() + " pills and won!", new Vector2(mid.X - 110, mid.Y - 20), Color.Black);
-                spriteBatch.DrawString(font, loser.getName() + " ate only " + loser.getScore() + " pills.", new Vector2(mid.X - 110, mid.Y), Color.Black);
+                spriteBatch.DrawString(font, loser.getName() + " ate only " + loser.getScore() + " pills.", new Vector2(mid.X - 100, mid.Y), Color.Black);
                 }
 
-            spriteBatch.DrawString(font, "Press Enter to continue", new Vector2(mid.X - 90, mid.Y + 30), Color.Red);
+            spriteBatch.DrawString(font, "Press Enter to continue", new Vector2(mid.X - 95, mid.Y + 26), Color.Red);
 
             }
 
